5dc5eedd7f9657ff5150120a7328427887594098
Evanlsmillan <evanschroedermil@ufl.edu>
2026-02-05 17:12:57 -0500

    Iitital commit

---
+++ src/tempSensor.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+static const uint8_t ONE_WIRE_BUS = 2; // GP2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1500); // give time for Serial Monitor to attach
+  Serial.println("Starting DS18B20 test...");
+
+  sensors.begin();
+
+  int count = sensors.getDeviceCount();
+  Serial.print("Devices found on OneWire bus: ");
+  Serial.println(count);
+
+  if (count == 0) {
+    Serial.println("No devices found. Check wiring + 4.7k pullup + pin number.");
+  }
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  float c = sensors.getTempCByIndex(0);
+
+  Serial.print("Temp C: ");
+  Serial.println(c);
+
+  delay(1000);
+}
