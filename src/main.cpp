8bc0156a9b270887ae4fa98b71e2207c5a4d3701
MateoC78 <166570320+MateoC78@users.noreply.github.com>
2026-03-09 19:51:49 -0400

    BMP test demo

--- src/main.cpp
+++ src/main.cpp
@@ -16,23 +16,50 @@
     //bmp.enablePressure(true);
 //configure interrupts (optional)
     //bmp.configureInterrupt(BMP5_LATCHED, BMP5_ACTIVE_HIGH, BMP5_INTR_PUSH_PULL, BMP5XX_INTERRUPT_DATA_READY, true);
-//perform reading of temperature and pressure
-    //bmp.performReading();
-    //bmp.pressure
+//perform reading of altitude
+    //bmp.readAltitude(1013.25);
 
 // put function declarations here:
+void initializeBMP();
 int myFunction(int, int);
+
+Adafruit_BMP5xx bmp;
+
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(9600);
+  while (!Serial);
+  initializeBMP();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (bmp.dataReady()) {
+    bmp.performReading();
+    float currAltitude = bmp.readAltitude(1013.25);
+    Serial.println(currAltitude);
+  }
 }
 
 // put function definitions here:
+void initializeBMP() {
+  if (!bmp.begin()) {
+    // Handle initialization failure
+    while (1) {
+      delay(100);
+    }
+  }
+
+  // Set oversampling, filter, ODR, power mode, and enable pressure measurement CHANGE AS DESIRED 
+  bmp.setPressureOversampling((bmp5xx_oversampling_t)BMP5_OVERSAMPLING_2X);
+  bmp.setIIRFilterCoeff((bmp5xx_iir_filter_t)BMP5_IIR_FILTER_COEFF_15);
+  bmp.setOutputDataRate((bmp5xx_odr_t)BMP5_ODR_50_HZ);
+  bmp.setPowerMode((bmp5xx_powermode_t)BMP5_POWERMODE_NORMAL);
+  bmp.enablePressure(true);
+}
+
 int myFunction(int x, int y) {
   return x + y;
 }
